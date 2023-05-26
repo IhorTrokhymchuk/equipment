ded179ae8d83442eb70702fec540bff037acd732
IhorTrokhymchuk <hameleon009@gmail.com>
2023-05-26 18:24:58 +0300

    add RentTest.java and last test

--- src/test/java/org/example/RentTest.java
+++ src/test/java/org/example/RentTest.java
@@ -29,4 +29,33 @@ public class RentTest {
         Assertions.assertEquals(1, rent.orderList.size());
         Assertions.assertEquals("Введіть ім'я:\nВиберіть вид техніки:\n1. Кран\n2. Бульдозер\n3. Екскаватор\nВиберіть тип оренди:\n1. Подобова\n2. Довгострокова\nЧи є постійний клієнт? (Так/Ні)\nЗамовлення створено успішно!\n", output.toString());
     }
+    @Test
+    public void testViewOrderWithNoOrders() {
+        rent.ViewOrder();
+        Assertions.assertEquals("У вас немає жодного замовлення.\n", output.toString());
+    }
+
+    @Test
+    public void testViewOrderWithOrders() {
+        rent.orderList.add(new Order("John", Equipment.Екскаватор, 24, 90.0, false));
+        rent.orderList.add(new Order("Jane", Equipment.Бульдозер, 48, 80.0, true));
+
+        rent.ViewOrder();
+        String expectedOutput = "Список замовлень:\n";
+        expectedOutput += "Замовлення: Ім'я: John, Вид техніки: Екскаватор, Тип оренди: Подобова, Тривалість: 24 год, Вартість: 2160.0 грн\n";
+        expectedOutput += "Замовлення: Ім'я: Jane, Вид техніки: Бульдозер, Тип оренди: Довгострокова, Тривалість: 48 год, Вартість: 3456.0 грн\n";
+
+        Assertions.assertEquals(expectedOutput, output.toString());
+    }
+
+    @Test
+    public void testGetPriceHour() {
+        double priceHour1 = rent.GetPriceHour(Equipment.Кран);
+        double priceHour2 = rent.GetPriceHour(Equipment.Бульдозер);
+        double priceHour3 = rent.GetPriceHour(Equipment.Екскаватор);
+
+        Assertions.assertEquals(100.0, priceHour1);
+        Assertions.assertEquals(80.0, priceHour2);
+        Assertions.assertEquals(90.0, priceHour3);
+    }
 }
